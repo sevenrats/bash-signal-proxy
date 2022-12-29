ff36c95fb054181ad1f9bbdc0199edd1e3ef2291
sevenrats <sevenrats@tutanota.com>
2022-12-28 21:32:52 -0500

    initial commit

---
+++ example-entrypoint.sh
@@ -0,0 +1,25 @@
+# Proxy Signals
+sp_processes=("tinyproxy") # These are processes that will receive all signals that aren't overloaded
+. ./signalproxy.sh
+
+# Overload specific handlers if you want to
+    # In this case lets overload all termination signals so that we can down wg0.
+_term() { 
+  echo "Caught a termination signal!"
+  wg-quick down wg0
+  pkill -TERM tinyproxy
+}
+
+trap _term SIGTERM
+trap _term SIGINT
+trap _term SIGQUIT
+#trap _term SIGKILL # You can't trap SIGKILL
+trap _term SIGHUP
+
+# Configure stuff
+
+#Launch App
+wg-quick up wg0 &&
+tinyproxy -dc /etc/tinyproxy/tinyproxy.conf & \
+wait -n
+
